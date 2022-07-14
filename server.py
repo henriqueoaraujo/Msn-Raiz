a273098ea7abd29a9c6a22dc7e244ad0ddfbaf23
henriqueoaraujo <henrique0araujo@hotmail.com>
2022-07-13 21:22:52 -0300

    Incluindo novas funções no servidor validação do servidor com o cliente, mensagem de boas vindas adicionada funcao de mensagem do cliente

--- server.py
+++ server.py
@@ -4,6 +4,8 @@ import socket
 
 from _thread import start_new_thread
 
+from xarray import broadcast
+
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
@@ -25,11 +27,63 @@ server.listen(100)
 list_of_clients = []
 
 
+def remove_cliente(cliente):
+    #Esta função remove o cliente da lista de cliente ativos
+
+    if cliente in list_of_clients:
+        list_of_clients.remove(cliente)
 
 
 
+def broadcast(mensagem, conexao):
+    #Esta função encaminha a mensagem de um cliente para todos 
+    for cliente in list_of_clients:
+        if cliente != conexao:
+        try:
+            cliente.send(mensagem)    
+        except:
+            cliente.close()
+
+            # se a conexao estiver quebrada, vamos remover o cliente dam inha lista de cliente ativos
+            remove_cliente(cliente)
 
 
+def thread_do_cliente (conexao, endereco):
+    # Mando mensagem de boas vindas atraves do objeto de conexão
+    conexao.send("Bem vindo ao chat Master Python".encode())    
+
+    while True:
+
+        try: 
+
+            mensagem = conexao.recv(2048)
+
+            if mensagem is not None:
+                #Print mensagem que o cliente mandou no terminal do servidor
+                mensagem_para_encaminhar = "[" + endereco[0] + "]: " + mensagem
+                
+                print(mensagem_para_encaminhar)
+
+                broadcast(mensagem_para_encaminhar, conexao)
+        except:
+            continue
+
+
+while True:
+    
+    """
+    Aceitar conexão de clientes
+     Objeto de conexao
+     Endereço de ip.
+    """
+    conexao, endereco = server.accept()
+  
+    #Salvar cliente na minha lista de clientes ativos
+    list_of_clients.append(conexao)
+
+    print("Cliente conectado endereço: " + endereco)
+
+    start_new_thread ( thread_do_cliente, (conexao, endereco))
 
 
 
